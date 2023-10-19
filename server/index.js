5408262924416c4771121229f3c60810e9e8a3dd
Rushda Ansari <iamrushdaa@gmail.com>
2023-10-19 21:28:36 +0530

    user routes

--- server/index.js
+++ server/index.js
@@ -8,9 +8,11 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-
-
-
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import { register } from "./controllers/auth.js";
+import { verifyToken } from "./middleware/auth.js";
 
 // CONFIGS //
  const __filename = fileURLToPath(import.meta.url);
@@ -37,6 +39,15 @@ const storage = multer.diskStorage({
 })
 const upload = multer({ storage });
 
+
+// Routes with files 
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost)
+
+// ROUTES
+app.use('/auth', authRoutes);
+app.use("/users", userRoutes);
+app.use("/post", postRoutes);
 
 // MONGOOSE SETUP // 
 
